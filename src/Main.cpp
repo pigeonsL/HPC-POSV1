adba900d0032c9deba7287ffea11dff730aa733a
JingeLi <jinge.li@thoughtworks.com>
2020-06-09 15:25:15 +0800

    implement main()

--- src/Main.cpp
+++ src/Main.cpp
@@ -2,7 +2,7 @@
 
 int main(int argc, char* argv[])
 {
-    std::vector<std::string> barCodes({"ITEM000001", "ITEM000003", "ITEM000001", "ITEM000005"});
+    std::vector<std::string> barCodes({"ITEM000001", "ITEM000003", "ITEM000001", "ITEM000005", "ITEM000003"});
     std::cout<<printReceipt(barCodes)<<std::endl;
     return 0;
 }
