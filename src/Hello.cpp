ec3cb13bf0141363eb712fbd3b7597478e32edea
JingeLi <jinge.li@thoughtworks.com>
2020-06-15 17:49:36 +0800

    refactor getOrder()

--- src/Hello.cpp
+++ src/Hello.cpp
@@ -14,11 +14,7 @@ std::map<size_t, size_t> getOrder(std::vector<std::string>& barcodes)
     for (int i = 0; i < barcodes.size(); ++i) {
         for (int j = 0; j < goods.size(); ++j) {
             if (barcodes[i] == goods[j].barcode) {
-                if (orders.count(j) == 0) {
-                    orders.insert(std::make_pair(j, 1));
-                }else{
-                    orders[j]++;
-                }
+                orders[j]++;
                 break;
             }
         }
@@ -27,7 +23,7 @@ std::map<size_t, size_t> getOrder(std::vector<std::string>& barcodes)
 }
 size_t countOrdersPrice(std::map<size_t, size_t>& orders){
     size_t totalPrice = 0;
-    for(std::map<size_t, size_t>::iterator it = orders.begin(); it != orders.end(); it++){
+    for(auto it = orders.begin(); it != orders.end(); it++){
         totalPrice += goods[it->first].price * it->second;
     }
     return totalPrice;
@@ -38,7 +34,7 @@ std::string printReceipt(std::vector<std::string>& barcodes){
 
     std::map<size_t, size_t> orders = getOrder(barcodes);
 
-    for(std::map<size_t, size_t>::iterator it = orders.begin(); it != orders.end(); it++){
+    for(auto it = orders.begin(); it != orders.end(); it++){
         receipt += goods[it->first].barcode + ", " + goods[it->first].name + ", x" +
                    std::to_string(it->second) + ", " + std::to_string(goods[it->first].price) + "\n";
     }
